9dc14b43185cbdec2b9b30b7bc10adb11837328c
Hensvik <872304091@qq.com>
2023-02-08 09:34:06 +0800

    question200岛屿的数量 question752打开转盘锁

---
+++ src/queueAndStack/question200岛屿的数量.java
@@ -0,0 +1,104 @@
+package queueAndStack;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+//给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+//
+//岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+//
+//此外，你可以假设该网格的四条边均被水包围。
+//
+public class question200岛屿的数量 {
+
+    //深度优先遍历方法DFS
+    public static int numIslands1(char[][] grid) {
+        int count = 0;
+
+        //外部循环
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(grid[i][j] == '1'){
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        System.out.println("count=" + count);
+        return count;
+    }
+
+    private static void dfs(char[][] grid, int i, int j){
+        //如果越界或者返回0
+        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0') {
+            return;
+        }
+        grid[i][j] = '0';
+        //4个方向，由于先前1的位置被置0，所以并不会出现重复统计的情况
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j - 1);
+    }
+
+    //广度优先遍历方法BDF
+    public static int numIslands2(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int num_islands = 0;
+
+        for (int r = 0; r < nr; ++r) {
+            for (int c = 0; c < nc; ++c) {
+                //当某个位置为1时，增加岛的数量，然后置为0
+                if (grid[r][c] == '1') {
+                    ++num_islands;
+                    grid[r][c] = '0';
+                    Queue<Integer> neighbors = new LinkedList<>();
+                    //将当前遍历到的位置的序号加入
+                    neighbors.add(r * nc + c);
+                    //当队列不为空时
+                    while (!neighbors.isEmpty()) {
+                        int id = neighbors.remove();
+                        int row = id / nc;
+                        int col = id % nc;
+
+                        //每次都需要把遍历到的序号加入，不然可能会超时
+
+                        if (row - 1 >= 0 && grid[row-1][col] == '1') {
+                            neighbors.add((row-1) * nc + col);
+                            grid[row-1][col] = '0';
+                        }
+                        if (row + 1 < nr && grid[row+1][col] == '1') {
+                            neighbors.add((row+1) * nc + col);
+                            grid[row+1][col] = '0';
+                        }
+                        if (col - 1 >= 0 && grid[row][col-1] == '1') {
+                            neighbors.add(row * nc + col-1);
+                            grid[row][col-1] = '0';
+                        }
+                        if (col + 1 < nc && grid[row][col+1] == '1') {
+                            neighbors.add(row * nc + col+1);
+                            grid[row][col+1] = '0';
+                        }
+                    }
+                }
+            }
+        }
+
+        return num_islands;
+    }
+
+    public static void main(String[] args) {
+        char grid[][] = {
+        {'1','1','1','1','0'},
+        {'1','1','0','1','0'},
+        {'1','1','0','0','0'},
+        {'0','0','0','0','0'}};
+        numIslands1(grid);
+        numIslands2(grid);
+    }
+}
---
+++ src/queueAndStack/question752打开转盘锁.java
@@ -0,0 +1,85 @@
+package queueAndStack;
+
+//你有一个带有四个圆形拨轮的转盘锁。每个拨轮都有10个数字： '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' 。每个拨轮可以自由旋转：例如把 '9' 变为 '0'，'0' 变为 '9' 。每次旋转都只能旋转一个拨轮的一位数字。
+//锁的初始数字为 '0000' ，一个代表四个拨轮的数字的字符串。
+//列表 deadends 包含了一组死亡数字，一旦拨轮的数字和列表里的任何一个元素相同，这个锁将会被永久锁定，无法再被旋转。
+//字符串 target 代表可以解锁的数字，你需要给出解锁需要的最小旋转次数，如果无论如何不能解锁，返回 -1 。
+//示例 1:
+//
+//输入：deadends = ["0201","0101","0102","1212","2002"], target = "0202"
+//输出：6
+//解释：
+//可能的移动序列为 "0000" -> "1000" -> "1100" -> "1200" -> "1201" -> "1202" -> "0202"。
+//注意 "0000" -> "0001" -> "0002" -> "0102" -> "0202" 这样的序列是不能解锁的，
+//因为当拨动到 "0102" 时这个锁就会被锁定。
+
+import java.util.*;
+
+public class question752打开转盘锁 {
+    public static int openLock(String[] deadends, String target) {
+        //如果目标为0000，特殊情况直接返回0
+        if("0000".equals(target)){
+            return 0;
+        }
+
+        //将死亡数字存入HashSet
+        Set<String> dead = new HashSet<String>();
+        for (String deadend : deadends) {
+            dead.add(deadend);
+        }
+
+        //如果死亡数字包含0000，直接返回-1
+        if (dead.contains("0000")) {
+            return -1;
+        }
+
+        int step = 0;
+        Queue<String> queue = new LinkedList<String>();
+        queue.offer("0000");
+        Set<String> seen = new HashSet<String>();
+        seen.add("0000");
+
+        //当队列不为空，
+        while (!queue.isEmpty()) {
+            ++step;
+            int size = queue.size();
+            for (int i = 0; i < size; ++i) {
+                String status = queue.poll();
+                for (String nextStatus : get(status)) {
+                    if (!seen.contains(nextStatus) && !dead.contains(nextStatus)) {
+                        if (nextStatus.equals(target)) {
+                            return step;
+                        }
+                        queue.offer(nextStatus);
+                        seen.add(nextStatus);
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public static char numPrev(char x) {
+        return x == '0' ? '9' : (char) (x - 1);
+    }
+
+    public static char numSucc(char x) {
+        return x == '9' ? '0' : (char) (x + 1);
+    }
+
+    // 枚举 status 通过一次旋转得到的数字
+    public static List<String> get(String status) {
+        List<String> ret = new ArrayList<String>();
+        char[] array = status.toCharArray();
+        for (int i = 0; i < 4; ++i) {
+            char num = array[i];
+            array[i] = numPrev(num);
+            ret.add(new String(array));
+            array[i] = numSucc(num);
+            ret.add(new String(array));
+            array[i] = num;
+        }
+        return ret;
+    }
+}
