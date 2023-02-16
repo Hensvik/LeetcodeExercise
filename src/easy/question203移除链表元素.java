65691872938c2b76643d66df381b7f2f48a06a3d
Hensvik <872304091@qq.com>
2023-02-16 22:33:53 +0800

    20220216

--- src/easy/question203移除链表元素.java
+++ src/easy/question203移除链表元素.java
@@ -1,10 +1,8 @@
 package easy;
 
 //给你一个链表的头节点 head 和一个整数 val ，请你删除链表中所有满足 Node.val == val 的节点，并返回 新的头节点 。
-// 
 //
 //示例 1：
-//
 //
 //输入：head = [1,2,6,3,4,5,6], val = 6
 //输出：[1,2,3,4,5]
@@ -16,7 +14,6 @@ package easy;
 //
 //输入：head = [7,7,7,7], val = 7
 //输出：[]
-// 
 //
 //提示：
 //
@@ -24,9 +21,6 @@ package easy;
 //1 <= Node.val <= 50
 //0 <= val <= 50
 //
-//来源：力扣（LeetCode）
-//链接：https://leetcode-cn.com/problems/remove-linked-list-elements
-//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
 public class question203移除链表元素 {
     public class ListNode {
