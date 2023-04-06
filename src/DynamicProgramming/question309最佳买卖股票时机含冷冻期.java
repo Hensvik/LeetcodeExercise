7b8a06774d3ec624721f02969ba417f6aca3e87f
Hensvik <872304091@qq.com>
2023-04-06 17:58:23 +0800

    20230406

--- src/DynamicProgramming/question309最佳买卖股票时机含冷冻期.java
+++ src/DynamicProgramming/question309最佳买卖股票时机含冷冻期.java
@@ -25,26 +25,51 @@ package DynamicProgramming;
 //而要定义成持有或者不持有的情况
 
 public class question309最佳买卖股票时机含冷冻期 {
-    public static int maxProfit(int[] prices) {
+    public static int maxProfit1(int[] prices) {
         int n = prices.length;
-        int [][]dp = new int[n][3];
+        int [][]dp = new int[n][4];
 
-        //不操作状态
-        dp[0][0]=0;
-        //买入
-        dp[0][1]=-prices[0];
-        //卖出
+        //持有股票的状态
+        dp[0][0]=-prices[0];
+        //保持卖出股票的状态
+        dp[0][1]=0;
+        //卖出股票的状态
         dp[0][2]=0;
+        //冷冻期
+        dp[0][3]=0;
 
         for (int i = 1; i < n; i++) {
-            dp[i][0]=dp[i-1][0];
-            dp[i][1]=Math.max(dp[i-1][1],-prices[i]);
-            dp[i][2]=Math.max(dp[i-1][2],dp[i-1][1]+prices[i]);
+            dp[i][0]=Math.max(dp[i-1][0],dp[i-1][3]-prices[i]);
+            dp[i][1]=dp[i-1][3];
+            dp[i][2]=Math.max(dp[i-1][0]+prices[i],dp[i-1][2]);
+            dp[i][3]=dp[i-1][2];
         }
+
+        return Math.max(dp[n-1][2],dp[n-1][3]);
+    }
+
+    //leetcode解法，三状态
+    public static int maxProfit2(int[] prices) {
+        if (prices.length == 0) {
+            return 0;
+        }
+
+        int n = prices.length;
+        // f[i][0]: 手上持有股票的最大收益
+        // f[i][1]: 手上不持有股票，并且处于冷冻期中的累计最大收益
+        // f[i][2]: 手上不持有股票，并且不在冷冻期中的累计最大收益
+        int[][] f = new int[n][3];
+        f[0][0] = -prices[0];
+        for (int i = 1; i < n; ++i) {
+            f[i][0] = Math.max(f[i - 1][0], f[i - 1][2] - prices[i]);
+            f[i][1] = f[i - 1][0] + prices[i];
+            f[i][2] = Math.max(f[i - 1][1], f[i - 1][2]);
+        }
+        return Math.max(f[n - 1][1], f[n - 1][2]);
     }
 
     public static void main(String[] args) {
         int []prices = {1,2,3,0,2};
-        maxProfit(prices);
+        maxProfit2(prices);
     }
 }
