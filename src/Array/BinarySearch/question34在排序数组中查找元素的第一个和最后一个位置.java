91b990d1c0933a435bbe60533d9a1e450d691205
Hensvik <872304091@qq.com>
2023-08-03 00:00:37 +0800

    20230803

--- src/Array/BinarySearch/question34在排序数组中查找元素的第一个和最后一个位置.java
+++ src/Array/BinarySearch/question34在排序数组中查找元素的第一个和最后一个位置.java
@@ -28,24 +28,44 @@ public class question34在排序数组中查找元素的第一个和最后一个
 
     //不确立左右开闭原则的话，很容易写成死循环或者交界位置无法处理
     public static int[] searchRange(int[] nums, int target) {
-        int []res = {-1,-1};
-
-        int L = 0;
-        int R = nums.length-1;
-
-        while(L<=R){
-            int mid = (L+R)/2;
-            if(nums[mid]<target){
-                L = mid+1;
-            }else{
-                R = mid-1;
+        int left = 0;
+        int right = nums.length - 1;
+        int first = -1;
+        int last = -1;
+        // 找第一个等于target的位置
+        while (left <= right) {
+            int middle = (left + right) / 2;
+            if (nums[middle] == target) {
+                //由于目标是找起始位置，所以此处设置first=middle，表示起始位置可能为mid或mid左侧
+                first = middle;
+                right = middle - 1; //重点
+            } else if (nums[middle] > target) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
             }
         }
 
-        res[0]=L;
-        res[1]=R;
-        return res;
+        // 最后一个等于target的位置
+        left = 0;
+        right = nums.length - 1;
+        while (left <= right) {
+            int middle = (left + right) / 2;
+            if (nums[middle] == target) {
+                //由于目标是找结束位置，所以此处设置last=middle，表示结束位置可能为mid或mid右侧
+                last = middle;
+                left = middle + 1; //重点
+            } else if (nums[middle] > target) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+        }
+
+        return new int[]{first, last};
     }
+
+
 
     public static void main(String[] args) {
         int []nums = {5,7,7,8,8,10};
